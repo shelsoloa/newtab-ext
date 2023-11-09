fc3df64628ecc8af5bf390b54d0ddb5cdb995404
Shel Soloa <shel@shelsoloa.com>
2023-11-08 23:03:03 -0500

    Add date & formatting

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
+function _leadingZero(num) {
+  return ('0' + num).slice(-2);
+}
+
 function updateTime() {
   let now = new Date();
 
+  const date_header = document.querySelector('#date-display');
   const time_header = document.querySelector("#time-display");
-  time_header.textContent = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
+
+  let date_string = 
+    now.getFullYear() + '-' +
+    _leadingZero(now.getMonth()) + '-' +
+    _leadingZero(now.getDate());
+
+  let time_string = 
+    _leadingZero(now.getHours()) + ':' +
+    _leadingZero(now.getMinutes()) + ':' +
+    _leadingZero(now.getSeconds());
+
+  date_header.textContent = date_string;
+  time_header.textContent = time_string;
 }
 
 window.onload = (event) => {
